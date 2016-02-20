15170e4a74410053b0a9dc2b76cf2e09dfb15e64
Junker <dk-junker@ya.ru>
2016-02-20 07:26:07 +0700

    removed some unnecessary dependencies

--- src/Junker/Silex/YamlFileLoader.php
+++ src/Junker/Silex/YamlFileLoader.php
@@ -4,8 +4,6 @@ namespace Junker\Silex;
 
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Config\Loader\FileLoader;
-use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
-use Symfony\Component\DependencyInjection\Exception\RuntimeException;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Parser as YamlParser;
@@ -42,15 +40,15 @@ class YamlFileLoader extends FileLoader
     protected function loadFile($file)
     {
         if (!class_exists('Symfony\Component\Yaml\Parser')) {
-            throw new RuntimeException('Unable to load YAML config files as the Symfony Yaml Component is not installed.');
+            throw new \RuntimeException('Unable to load YAML config files as the Symfony Yaml Component is not installed.');
         }
 
         if (!stream_is_local($file)) {
-            throw new InvalidArgumentException(sprintf('This is not a local file "%s".', $file));
+            throw new \InvalidArgumentException(sprintf('This is not a local file "%s".', $file));
         }
 
         if (!file_exists($file)) {
-            throw new InvalidArgumentException(sprintf('The service file "%s" is not valid.', $file));
+            throw new \InvalidArgumentException(sprintf('The service file "%s" is not valid.', $file));
         }
 
         if ($this->yamlParser === null) {
@@ -60,7 +58,7 @@ class YamlFileLoader extends FileLoader
         try {
             $data = $this->yamlParser->parse(file_get_contents($file));
         } catch (ParseException $e) {
-            throw new InvalidArgumentException(sprintf('The file "%s" does not contain valid YAML.', $file), 0, $e);
+            throw new \InvalidArgumentException(sprintf('The file "%s" does not contain valid YAML.', $file), 0, $e);
         }
 
         return $data;
@@ -72,14 +70,14 @@ class YamlFileLoader extends FileLoader
             return;
         }
         if (!is_array($config->data['imports'])) {
-            throw new InvalidArgumentException(sprintf('The "imports" key should contain an array in %s. Check your YAML syntax.', $file));
+            throw new \InvalidArgumentException(sprintf('The "imports" key should contain an array in %s. Check your YAML syntax.', $file));
         }
 
         $defaultDirectory = dirname($file);
 
         foreach ($config->data['imports'] as $import) {
             if (!is_array($import)) {
-                throw new InvalidArgumentException(sprintf('The values in the "imports" key should be arrays in %s. Check your YAML syntax.', $file));
+                throw new \InvalidArgumentException(sprintf('The values in the "imports" key should be arrays in %s. Check your YAML syntax.', $file));
             }
 
             $this->setCurrentDir($defaultDirectory);
